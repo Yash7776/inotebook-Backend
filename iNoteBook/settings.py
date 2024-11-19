5a8014cf27ec531ea14a5db59ad3080188d5ce3f
Yash Pathak <106083687+Ayush7776@users.noreply.github.com>
2024-11-19 17:13:31 +0530

    Update settings.py

--- iNoteBook/settings.py
+++ iNoteBook/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-=^z=@vjqzgg49^8_rfgdcl1--za4eae2vg88c6u!arp&=e3bc!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
